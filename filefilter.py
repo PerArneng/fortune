e99d3a8d0b722281d22a00dfa1558135bffc8ba0
Per Arneng <per.arneng@scalebit.com>
2014-11-15 19:50:18 +0100

    Create filefilter.py

---
+++ filefilter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import os
+import sets
+
+def is_file(file):
+	if file is None:
+		return false
+	return os.path.isfile(file)
+
+def extract_path(pattern, line, group_nr):
+	match = pattern.search(line)
+	path = None
+	if match:
+		file = match.group(group_nr)
+		if is_file(file):
+			path = file	
+	return path
+
+
+def main():
+
+	if len(sys.argv) < 2:
+		print "usage: filefilter <(strace|ldd) output...>"
+		exit(1)
+
+
+	for arg_file in sys.argv[1:]:
+
+		output_file = open(arg_file, "r")
+
+		path_set = sets.Set()
+
+		strace_pattern = re.compile('(open|stat|execve)\("([^"]+)"')
+		ldd_pattern = re.compile('(/.*) \(0x\w+\)$')
+
+		for line in output_file:
+
+			path = extract_path(strace_pattern, line, 2)
+			if path is not None:
+				path_set.add(path)
+
+			path = extract_path(ldd_pattern, line, 1)
+			if path is not None:
+				path_set.add(path)
+
+		for path in path_set:
+			print path
+
+
+if __name__ == "__main__":
+	main()
+
